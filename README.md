e43b4a65f508cef0777e22d982bf5d9cb454aa54
Electric Ant <electricant@anche.no>
2020-12-05 22:46:38 +0100

    Use SPIFFS to configure each sensor node.

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Contains the firmware for the Arduino IDE.
 
 ### Required Libraries:
 **TODO**
+
+To write data into ESP8266 SPIFFS the
+[Arduino ESP8266 filesystem uploader](https://github.com/esp8266/arduino-esp8266fs-plugin)
+plugin is required.
+
+After flashing the firmware rename data/config-example.ini to data/config.ini
+and edit it to suit your flavour.
--- cece-firmware/PostTempSensor/PostTempSensor.ino
+++ cece-firmware/PostTempSensor/PostTempSensor.ino
@@ -12,37 +12,36 @@ extern "C" {
   #include "user_interface.h"
 }
 #include <elapsedMillis.h>
-
+#include "FS.h"
+#include <SPIFFSIniFile.h>
 #include <Wire.h>
+
 #include "SparkFunHTU21D.h"
 HTU21D myHumidity;
 
+// Constant used to convert minutes to milliseconds
 #define MINS_TO_MILLIS (60*1000)
-
-/*
- * Node configuration
- */
-#define POST_ADDRESS "iot.pielluzza.ts"
-#define POST_ENDPOINT "http://" POST_ADDRESS "/cgi/post_data.pl"
-
-#define STASSID ""
-#define STAPSK  ""
-
-#define NODE_ID "cece-test"
-//#define NODE_ID "cece-stanza"
-//#define NODE_ID "cece-fuori"
-
-#define SLEEP_MINS 1
-
+// Baud rate for the UART port
+#define UART_BAUD_RATE 115200
+// Buffer length used for reading the config.ini file
+#define INI_BUF_LENGTH 80
+// Buffer length used for storing string values read from config.ini
+#define VAL_BUF_LENGTH 50
+// Builtin LED brightness when on
 #define LED_ON_BRIGHTNESS_PERCENT 8
 
 /*
- * Routines
+ * The following variables are read from SPIFFS during init.
  */
+int measurement_interval_min = 1; // Measurement interval in minutes
+char node_name[VAL_BUF_LENGTH] = "A Horse with No Name"; // node name
+char post_endpoint[VAL_BUF_LENGTH]; // endpoint address
 
-// The controls for the builtin LED are reversed.
-// Wen the pin is set to HIGH the LED is off and vice-versa.
-// This subroutine makes it easy to set the brightness appropriately
+/* 
+ *  The controls for the builtin LED are reversed.
+ *  When the pin is set to HIGH the LED is off and vice-versa.
+ * This subroutine makes it easy to set the brightness appropriately
+ */
 inline void setBuiltinLEDBrightness(int percent) __attribute__((always_inline));
 void setBuiltinLEDBrightness(int percent)
 {
@@ -61,10 +60,10 @@ void readDataAndSend()
   WiFiClient client;
   HTTPClient http;
   
-  http.begin(client, POST_ENDPOINT);
+  http.begin(client, post_endpoint);
   http.addHeader("Content-Type", "application/json");
 
-  String post_str = (String)"{\"ID\": \"" + NODE_ID + "\"," +
+  String post_str = (String)"{\"ID\": \"" + node_name + "\"," +
                              "\"temp\": " + temp + "," +
                              "\"rhum\": " + humd + "}";
                                
@@ -84,48 +83,68 @@ void readDataAndSend()
 }
 
 void setup()
-{  
+{
+  char buffer[INI_BUF_LENGTH]; // buffer for reading config file
+  char wifi_ssid[VAL_BUF_LENGTH];
+  char wifi_psk[VAL_BUF_LENGTH];
+   
   // initialize onboard LED as output and turn it on
   pinMode(BUILTIN_LED, OUTPUT);
   digitalWrite(BUILTIN_LED, LOW);
 
   // initialize serial
-  Serial.begin(115200);
+  Serial.begin(UART_BAUD_RATE);
   Serial.println("");
 
+  // read config.ini file from SPIFFS and initialize variables
+  if (!SPIFFS.begin())
+  {
+      Serial.println("ERROR: SPIFFS.begin() failed. Stopping.");
+      while(1) ;
+  }
+  
+  SPIFFSIniFile ini("/config.ini");
+  if (!ini.open())
+  {
+    Serial.println("ERROR: config.ini does not exist. Stopping.");
+    while (1) ;
+  }
+
+  if (!ini.validate(buffer, INI_BUF_LENGTH)) {
+    Serial.print("ERROR: config.ini not valid. Error: ");
+    Serial.print(ini.getError());
+    Serial.println(". Stopping.");
+    while (1) ;
+  }
+  ini.getValue("node", "name", buffer, INI_BUF_LENGTH, node_name, VAL_BUF_LENGTH);
+  ini.getValue("node", "endpoint", buffer, INI_BUF_LENGTH, post_endpoint, VAL_BUF_LENGTH);
+  ini.getValue("node", "meas_interval_min", buffer, INI_BUF_LENGTH, measurement_interval_min);
+  ini.getValue("wifi", "ssid", buffer, INI_BUF_LENGTH, wifi_ssid, VAL_BUF_LENGTH);
+  ini.getValue("wifi", "psk", buffer, INI_BUF_LENGTH, wifi_psk, VAL_BUF_LENGTH);
+  
   // initialize wifi
-  WiFi.hostname(NODE_ID);
-  WiFi.begin(STASSID, STAPSK);
   // Force the ESP into client-only mode (otherwhise light sleep cannot be enabled)
   WiFi.mode(WIFI_STA); 
+  WiFi.hostname(node_name);
+
   // Enable light sleep
   wifi_set_sleep_type(LIGHT_SLEEP_T);
 
-  Serial.print("[WiFi] Connecting");
+  Serial.print("[WiFi] Connecting to ");
+  Serial.print(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_psk);
   while (WiFi.status() != WL_CONNECTED)
   {
     digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
     delay(500);
     Serial.print(".");
   }
-  Serial.print("Connected! RSSI = ");
+  Serial.print("\n[WiFi] Connected. RSSI = ");
   Serial.print(WiFi.RSSI());
   Serial.println(" dBm");
   Serial.print("[WiFi] IP address: ");
   Serial.println(WiFi.localIP());
-  /*
-  Serial.print("[WiFi] DNS servers: ");
-  WiFi.dnsIP().printTo(Serial);
-  Serial.print(", ");
-  WiFi.dnsIP(1).printTo(Serial);
-  Serial.println();
-  
-  Serial.print("[WiFi] " POST_ADDRESS);
-  Serial.print(" is at ");
-  IPAddress resolvedIP;
-  WiFi.hostByName(POST_ADDRESS, resolvedIP);
-  Serial.println(resolvedIP);
-  */
+ 
   // initialize HTU21D temp sensor connected as in the following code
   // https://github.com/enjoyneering/HTU21D/blob/master/examples/HTU21D_Demo/HTU21D_Demo.ino
   myHumidity.begin();
@@ -145,5 +164,5 @@ void loop()
   }
   
   setBuiltinLEDBrightness(0);
-  delay((SLEEP_MINS * MINS_TO_MILLIS) - elapsed); // delay uses millisecond. We'd like to sleep for minutes
+  delay((measurement_interval_min * MINS_TO_MILLIS) - elapsed); // delay uses millisecond. We'd like to sleep for minutes
 }
